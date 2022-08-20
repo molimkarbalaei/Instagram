216828b9cc283496f08c35e76be0f7611c625145
molim <mohade3.karbalaei@gmail.com>
2022-08-20 15:10:32 +0200

    feat: add timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,29 @@
+// we need to get the logged in user's photos
+// on loading the photos, we need to use react skeleton
+// if we need photos, render them(create a post component)
+// if the user has no photo, tell them create some
+
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import usePhotos from "../hook/use-photos";
+
 export default function Timeline() {
+  // 1--- we need to get the logged in user's photos
+  const { photos } = usePhotos();
+
   return (
-    <div className="container col-span-4">
-      <p>i am timeliiiine</p>
+    //************************ inja span ro khodesh 2 gozashte */
+    <div className="container col-span-2">
+      {/* 2----- on loading the photos, we need to use react skeleton */}
+      {/* if no photos go ahead and  */}
+      {!photos ? (
+        <Skeleton count={10} width={640} height={500} className="mt-5" />
+      ) : photos?.length > 0 ? (
+        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+      ) : (
+        <p className="text-center text-2xl">Follow people to see photos</p>
+      )}
     </div>
   );
 }
+// we rerutn back photos as an obj and u have photos inside and inside photos you have array.
