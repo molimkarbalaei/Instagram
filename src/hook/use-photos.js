69c4e8bf6e237161ec5f3e2060e8fd4dca26df90
molim <mohade3.karbalaei@gmail.com>
2022-08-20 15:11:57 +0200

    feat: add custoom hook

---
+++ src/hook/use-photos.js
@@ -0,0 +1,39 @@
+// we create our own costum hook:
+import { useState, useEffect, useContext } from "react";
+// we have to have a create something in servisec that allows us
+// to get photos so we need user===> UserContext:
+import UserContext from "../context/user";
+import { getUserByUserId, getPhotos } from "../services/firebase";
+
+export default function usePhotos() {
+  const [photos, setPhotos] = useState(null);
+  const {
+    user: { uid: userId = "" },
+  } = useContext(UserContext);
+
+  useEffect(() => {
+    async function getTimelinePhotos() {
+      //examples: [2 , 5 , 1] => 2 is raphel
+      // we need the get the user by userid:
+      const [{ following }] = await getUserByUserId(userId);
+      // we have an arry that gonna be a URL such imgsources
+      let followedUserPhotos = [];
+
+      // we have to check does the user follow people?
+      if (following.length > 0) {
+        followedUserPhotos = await getPhotos(userId, following);
+      }
+
+      // display the photos in a formal way:
+      //unix epoch:
+      followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated);
+      // now we have newest photo in the feed.
+      setPhotos(followedUserPhotos);
+    }
+
+    getTimelinePhotos();
+  }, [userId]);
+  return { photos };
+}
+
+// we return photos as an obj  ==>  return { photos };
